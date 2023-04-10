48f7b79fa962932b27029bdf67f555cf8499d454
Maksym Khimii <meizovicmaks@gmail.com>
2023-04-10 12:53:17 +0300

    renaming module polls -> shop

--- mysite/urls.py
+++ mysite/urls.py
@@ -6,6 +6,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('polls.urls')),
+    path('', include('shop.urls')),
 
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
